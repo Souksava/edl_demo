5666b75a7a273dff1c5e4af278ad22d79d47dba5
nilakone sivilay <souksavath.souksavath.52221881@gmail.com>
2020-12-03 12:46:44 +0700

    edl

---
+++ administrator/Main.php
@@ -0,0 +1,27 @@
+<?php
+  $title = "ໜ້າຫຼັກ";
+  $path = "../";
+  $links = "";
+  $session_path = "../";
+  include ("../header-footer/header.php");
+?>
+  <div class="row">
+
+    <a href="customer" class="m-a">
+      <div class="btn btn-light mainlink">
+          ລູກຄ້າ
+      </div>
+    </a>
+
+    <a href="bill" class="m-a">
+      <div class="btn btn-light mainlink">
+          ໃບບິນ
+      </div>
+    </a>
+  </div>
+ 
+<?php
+ include ("../header-footer/footer.php");
+?>
+
+  
