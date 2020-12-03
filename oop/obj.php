01a15210fbda0fd045452ee61bd77e2bbf9089fe
Souksava <50312071+Souksava@users.noreply.github.com>
2020-12-03 16:44:36 +0700

    comit customer

--- oop/obj.php
+++ oop/obj.php
@@ -59,6 +59,71 @@ class obj{
                      }
                  }    
     }
+    public static function category(){
+        global $conn;
+        global $result_cate;
+        $result_cate = mysqli_query($conn,"select * from category order by cate_name asc");
+    }
+    public static function province(){
+        global $conn;
+        global $result_pro;
+        $result_pro = mysqli_query($conn,"select * from province order by pro_name asc");
+    }
+    public static function select_cus($search){
+        global $conn;
+        global $result_cus;
+        $result_cus = mysqli_query($conn,"select cus_id,cus_name,cus_surname,tel,address,c.pro_id,pro_name,c.cate_id,cate_name,gender,delivery,cus_no from customer c left join province p on c.pro_id=p.pro_id left join category d on c.cate_id=d.cate_id where cus_id like '$search' or cus_name like '$search' or gender like '$search' or pro_name like '$search' or cate_name like '$search' or cus_no like '$search' order by cus_name asc");
+    }
+    public static function insert_cus($cus_id,$cus_name,$cus_surname,$tel,$address,$pro_id,$cate_id,$gender,$delivery,$cus_no){
+        global $conn;
+        $result = mysqli_query($conn,"insert into customer values('$cus_id','$cus_name','$cus_surname','$tel','$address','$pro_id','$cate_id','$gender','$delivery','$cus_no')");
+        if(!$result){
+            echo"<script>";
+            echo"window.location.href='customer?save=fail';";
+            echo"</script>";
+        }
+        else{
+            echo"<script>";
+            echo"window.location.href='customer?save2=success';";
+            echo"</script>";
+        }
+    }
+    public static function update_cus($cus_id,$cus_name,$cus_surname,$tel,$address,$pro_id,$cate_id,$gender,$delivery,$cus_no){
+        global $conn;
+        $result = mysqli_query($conn,"update customer set cus_name='$cus_name',cus_surname='$cus_surname',tel='$tel',address='$address',pro_id='$pro_id',cate_id='$cate_id',gender='$gender',delivery='$delivery',cus_no='$cus_no' where cus_id='$cus_id'");
+        if(!$result){
+            echo"<script>";
+            echo"window.location.href='customer?update=fail';";
+            echo"</script>";
+        }
+        else{
+            echo"<script>";
+            echo"window.location.href='customer?update2=success';";
+            echo"</script>";
+        }
+    }
+    public static function delete_cus($cus_id){
+        global $conn;
+        $resultck = mysqli_query($conn,"select * from sell where cus_id='$cus_id'");
+        if(!$resultck){
+            echo"<script>";
+            echo"window.location.href='customer?delete=fail';";
+            echo"</script>";
+        }
+        else{
+            $result = mysqli_query($conn,"delete from customer where cus_id='$cus_id'");
+            if(!$result){
+                echo"<script>";
+                echo"window.location.href='customer?del=fail';";
+                echo"</script>";
+            }
+            else{
+                echo"<script>";
+                echo"window.location.href='customer?del2=success';";
+                echo"</script>";
+            }
+        }
+    }
 }
 $obj = new obj();
 // $obj->cookie_stock('1','2','3','4');
